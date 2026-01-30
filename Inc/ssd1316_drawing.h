6803af1f98ea766a9bb4109cbe4f7c91cbf5d482
cshapard26 <152207773+cshapard26@users.noreply.github.com>
2026-01-30 17:38:42 -0600

    Add ssd1316_drawing.h for shape drawing functions
    This header file declares functions for drawing shapes on the SSD1316 display.

---
+++ Inc/ssd1316_drawing.h
@@ -0,0 +1,22 @@
+/*
+ * ssd1316_drawing.h
+ *
+ *  Created on: Jan 28, 2026
+ *      Author: cshapard26
+ */
+
+#ifndef SSD1316_DRAWING_H_
+#define SSD1316_DRAWING_H_
+
+#include "ssd1316_core.h"
+
+
+uint8_t SSD1316_Draw_Point(uint8_t x, uint8_t y, bool color);
+uint8_t SSD1316_Draw_Line(uint8_t* points, bool color);
+
+uint8_t SSD1316_Draw_Rectangle(uint8_t* dimensions, bool filled, bool append);
+uint8_t SSD1316_Draw_Triangle(uint8_t* dimensions, bool filled, bool append);
+uint8_t SSD1316_Draw_Circle(uint8_t* dimensions, bool filled, bool append);
+
+
+#endif /* SSD1306_INC_SSD1316_DRAWING_H_ */
