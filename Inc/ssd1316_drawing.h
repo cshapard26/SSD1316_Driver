52addec78895de57b71e5aee5df2a8a9e2304a32
cshapard26 <152207773+cshapard26@users.noreply.github.com>
2026-01-31 22:00:46 -0600

    Add new drawing functions and update parameters

--- Inc/ssd1316_drawing.h
+++ Inc/ssd1316_drawing.h
@@ -14,9 +14,11 @@
 uint8_t SSD1316_Draw_Point(uint8_t x, uint8_t y, bool color);
 uint8_t SSD1316_Draw_Line(uint8_t* points, bool color);
 
-uint8_t SSD1316_Draw_Rectangle(uint8_t* dimensions, bool filled, bool append);
-uint8_t SSD1316_Draw_Triangle(uint8_t* dimensions, bool filled, bool append);
-uint8_t SSD1316_Draw_Circle(uint8_t* dimensions, bool filled, bool append);
+uint8_t SSD1316_Draw_X_Gon(uint8_t* dimensions, uint8_t edges, bool color);
+
+uint8_t SSD1316_Draw_Rectangle(uint8_t* dimensions, bool color);
+uint8_t SSD1316_Draw_Triangle(uint8_t* dimensions, bool color);
+uint8_t SSD1316_Draw_Circle(uint8_t* dimensions, bool color);
 
 
 #endif /* SSD1306_INC_SSD1316_DRAWING_H_ */
